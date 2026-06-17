1703361a51fc37fb7a7e001a76fcc90b43ebeafc
abhijitbora103 <abora7344@gmail.com>
2026-06-17 13:37:02 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import Admin from './Admin.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const path = window.location.pathname;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {path === '/admin' ? <Admin /> : <App />}
+  </React.StrictMode>,
+)
