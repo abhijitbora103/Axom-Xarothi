bfa8a0e0621dbf58d0a22a3b78667acf217ce01e
abhijitbora103 <abora7344@gmail.com>
2026-06-17 13:52:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import Admin from './Admin.tsx';
-import './index.css'; // এইটো ডিজাইনৰ বাবে বহুত দৰকাৰী
+import './index.css'; 
 
 const path = window.location.pathname;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {path === '/admin' ? <Admin /> : <App />}
+    {/* path === ৰ সলনি path.includes ব্যৱহাৰ কৰা হৈছে যাতে /admin/ থাকিলেও কাম কৰে */}
+    {path.includes('/admin') ? <Admin /> : <App />}
   </StrictMode>,
 );
